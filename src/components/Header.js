1b127860974fe8e27078d85756b669396c9991ac
Niel Cansino <nielcansino@gmail.com>
2020-05-09 15:41:07 +0900

    removed Trial.js and dependencies to it; fixed build warnings to pass Travis build and standard linter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
-import SocialMediaButtons from '../components/SocialMediaButtons';
+import SocialMediaButtons from '../components/SocialMediaButtons'
 
 const styles = theme => ({
   root: {
-    flexGrow: 0,
+    flexGrow: 0
   },
   button: {
     margin: theme.spacing.unit,
-    color: '	rgb(240,240,240)'
+    color: 'rgb(240,240,240)'
   },
   button_current: {
     margin: theme.spacing.unit,
-    color: '	rgb(192,192,192)'
+    color: 'rgb(192,192,192)'
   },
   button_home: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
-  appbar:{
+  appbar: {
     display: 'flex',
-    background:'linear-gradient(to bottom, rgb(0,0,0, 0.45), rgb(0,0,0, 0))',
-    boxShadow: "none",
-    position:'absolute',
+    background: 'linear-gradient(to bottom, rgb(0,0,0, 0.45), rgb(0,0,0, 0))',
+    boxShadow: 'none',
+    position: 'absolute',
     height: 'calc(10vh)'
   },
   hMenu: {
     display: 'flex',
     flexGrow: 1,
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   bTitle: {
     color: 'rgb(240,240,240)',
@@ -42,49 +42,55 @@ const styles = theme => ({
     font: '500 2vmin/1vh roboto'
   },
   smbutton: {
-    margin: theme.spacing.unit,
-  },
-});
-
-
+    margin: theme.spacing.unit
+  }
+})
 
 class Header extends Component {
-  bandler = (index) => {
+  bandler = index => {
     return () => {
-      this.props.onClick(index);
-      console.log('Clicked button ' + index + 'of the menu');
+      this.props.onClick(index)
+      console.log('Clicked button ' + index + 'of the menu')
     }
-  };
+  }
 
-  render() {
-    const {classes} = this.props;
+  render () {
+    const { classes } = this.props
 
-    const headerMenu=['WELCOME', 'WORKS','BLOGS','CONTACT'];//change this to add headerMenu
+    const headerMenu = ['WELCOME', 'WORKS', 'BLOGS', 'CONTACT'] // change this to add headerMenu
     return (
       <div className={classes.root}>
-        <AppBar position="static" className={classes.appbar}>
+        <AppBar position='static' className={classes.appbar}>
           <Toolbar>
-            <Button href="/"><Typography className={classes.bTitle}>JAY CANSINO BLOG</Typography></Button>
+            <Button href='/'>
+              <Typography className={classes.bTitle}>
+                JAY CANSINO BLOG
+              </Typography>
+            </Button>
             <div className={classes.hMenu}>
-              {
-                headerMenu.map((item,index) =>
-                <Button className={this.props.sliderSelectedItem==index ? classes.button_current : classes.button}
-                  onClick={this.bandler(index)}>
+              {headerMenu.map((item, index) => (
+                <Button
+                  className={
+                    this.props.sliderSelectedItem === index
+                      ? classes.button_current
+                      : classes.button
+                  }
+                  onClick={this.bandler(index)}
+                >
                   {item}
                 </Button>
-              )
-              }
+              ))}
             </div>
             <SocialMediaButtons />
           </Toolbar>
         </AppBar>
       </div>
-    );
+    )
   }
 }
 
 Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(Header);
+export default withStyles(styles)(Header)
--- src/containers/Navigator.js
+++ src/containers/Navigator.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Segment from '../components/Segment'
 import SliderContainer from '../components/SliderContainer'
 
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles'
 
 const styles = {
   root: {
@@ -13,32 +11,36 @@ const styles = {
     width: '100%',
     height: '100vh'
   }
-};
+}
 
 class Navigator extends Component {
   state = {
-    sliderSelectedItem: 0,
-  };
-
-  changeSelectedItem = (index) => {
-    console.log('received index '+ index);
-    this.setState(
-      {
-        sliderSelectedItem: index
-      },
-    );
+    sliderSelectedItem: 0
   }
 
-  render() {
-    const {classes} = this.props;
+  changeSelectedItem = index => {
+    console.log('received index ' + index)
+    this.setState({
+      sliderSelectedItem: index
+    })
+  }
+
+  render () {
+    const { classes } = this.props
     return (
       <div className={classes.root}>
-        <Header sliderSelectedItem={this.state.sliderSelectedItem} onClick={this.changeSelectedItem}/>
-        <SliderContainer sliderSelectedItem={this.state.sliderSelectedItem}  onClick={this.changeSelectedItem}/>
+        <Header
+          sliderSelectedItem={this.state.sliderSelectedItem}
+          onClick={this.changeSelectedItem}
+        />
+        <SliderContainer
+          sliderSelectedItem={this.state.sliderSelectedItem}
+          onClick={this.changeSelectedItem}
+        />
         <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default withStyles(styles)(Navigator);
+export default withStyles(styles)(Navigator)
